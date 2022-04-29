c986ec1ef483b3f7652d46e1f56774970dcd2287
김진일 <jiniljeil1@naver.com>
2022-04-30 00:10:22 +0900

    update

--- converter/src/main/java/com/handong/oh318/Extractor/Extractor.java
+++ converter/src/main/java/com/handong/oh318/Extractor/Extractor.java
@@ -8,21 +8,6 @@ import java.util.HashMap;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 
 public class Extractor {
-    
-    // public HashMap<String, JavaClassSource> getJavaClassSources(String path, String fileExtension) { 
-    //     HashMap<String, JavaClassSource> classes = new HashMap<String, JavaClassSource>() ; 
-
-    //     try {
-    //         ArrayList<Path> paths = (ArrayList<Path>) Util.getJavaFilepaths(path, fileExtension);
-
-    //         for (Path p : paths) {
-    //             classes.put(p.toString(), Util.getJaveClassSource(p.toString())) ; 
-    //         }
-    //     } catch (IOException e) {
-    //         System.err.println("Cannot find the path") ; 
-    //     }
-    //     return classes; 
-    // }   
 
     public ArrayList<JavaClassSource> getJavaClassSources(String path, String fileExtension) { 
         ArrayList<JavaClassSource> classes = new ArrayList<>() ; 
